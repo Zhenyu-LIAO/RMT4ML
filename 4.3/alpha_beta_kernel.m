bb1199ef3b35052aa6191ccfe6da9a04e4f3e31b
Zhenyu Liao <raymondedge928@gmail.com>
2021-07-17 14:26:27 +0800

    update

--- 4.3/alpha_beta_kernel.m
+++ 4.3/alpha_beta_kernel.m
@@ -1,4 +1,4 @@
-%% Section 4.3 The $\alpha-\beta$ random kernel model
+%% Section 4.3 The $\alpha-\beta$ random kernel matrix
 % This page contains simulations in Section 4.3.
 
 %% Eigenspectrum of $\Phi$
@@ -9,7 +9,6 @@ p = 400*coeff;
 n = 1200*coeff;
 c = p/n;
 
-rng(928);
 W = randn(p,n); % population covariance = identity
 
 Phi = sqrt(p)*( (W'*W/p).^2 - 1/p);
@@ -23,29 +22,33 @@ spike_location = Omega/c+omega^2/Omega;
 edges = linspace(-2*omega/sqrt(c), 2*omega/sqrt(c), 100);
 mu = sqrt( 4*omega^2/c - edges.^2 )*c/2/pi/omega^2;
 
+
 figure
 hold on
-histogram(eigs_Phi, 50, 'Normalization', 'pdf');
-plot(edges,mu, 'r', 'Linewidth',2);
-plot([-spike_location, spike_location],[0,0],'x', 'LineWidth', 2);
+histogram(eigs_Phi, 50, 'Normalization', 'pdf', 'EdgeColor', 'white');
+plot(edges,mu, 'r', 'Linewidth', 2);
+plot([-spike_location, spike_location],[0,0],'rx', 'LineWidth', 2);
 legend('Empirical eigenvalues', 'Rescaled semi-circle law', 'FontSize', 15, 'Interpreter', 'latex');
 axis([-7 7 0 0.16]);
 
-%%
 [V,eigs_Phi] = eig(Phi,'vector');
 [~,ind] = sort(eigs_Phi);
 eigs_Phi = eigs_Phi(ind);
 V = V(:,ind);
 
-figure
-subplot(1,2,1)
-plot(V(:,1))
-yline(mean(V(:,1)),'--');
-subplot(1,2,2)
-plot(V(:,n))
-yline(mean(V(:,n)),'--');
+step_size = 5;
 
-%% Limiting spectra of $\alpha-\beta$ kernel
+figure
+subplot(2,1,1)
+plot(V(1:step_size:end,1))
+yline(mean(V(:,1)),'--');
+title('Bottom eigenvector', 'Interpreter', 'latex');
+subplot(2,1,2)
+plot(V(1:step_size:end,n))
+yline(mean(V(:,n)),'--');
+title('Top eigenvector', 'Interpreter', 'latex');
+
+%% Limiting spectrum of $\alpha-\beta$ kernel with different $\alpha$ and $\beta$
 close all; clear; clc
 
 coeff = 1;
@@ -57,7 +60,7 @@ cs = [1/2, 1/2];
 n_cs = n*cs;
 k = length(cs); % number of classes
 
-alpha = 8; beta = 1;
+alpha = 8; beta = 1; % to change to see from a MP tp SC transition
 f = @(t) beta*(t+alpha/2/beta/sqrt(p)).^2;
 f_d = @(t) 2*beta*(t+alpha/2/beta/sqrt(p));
 
@@ -85,7 +88,6 @@ covs_mean = cs(1)*covs(1) + cs(2)*covs(2);
 eigs_covs_mean = eig(covs_mean);
 tau = trace(covs_mean)/p;
 
-rng(928);
 W=zeros(p,n);
 for i=1:k
     W(:,sum(cs(1:(i-1)))*n+1:sum(cs(1:i))*n)=sqrtm(covs(i))*randn(p,cs(i)*n);
@@ -131,7 +133,7 @@ for j=1:length(zs)
 end
 
 figure
-histogram(eigs_rescale_PKP, 40, 'Normalization', 'pdf');
+histogram(eigs_rescale_PKP, 40, 'Normalization', 'pdf', 'EdgeColor', 'white');
 hold on;
 plot(edges,mu,'r', 'Linewidth',2);
 legend('Empirical spectrum of $\check K$', 'Limiting law', 'FontSize', 15, 'Interpreter', 'latex');
